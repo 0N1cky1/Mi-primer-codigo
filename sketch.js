b4d50d56aa05ce86e59c72bf407442b0204bd0a3
0N1cky1 <160347524+0N1cky1@users.noreply.github.com>
2024-02-17 14:24:14 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500, 500);
+background('rgba(0,255,0, 0.25)');
+}
+
+function draw() {
+    ellipse( 250, 250, 400, 240,);
+  circle(250,250,250);
+  circle(250,250,120);
+
+  
+}
